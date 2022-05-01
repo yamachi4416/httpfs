94db29f793fd0f7d721b57449ced3fc0f550bf05
yamachi4416 <yamachi4416@gmail.com>
2022-05-01 10:37:51 +0900

    Files.vueをsetupスクリプトに変更

--- httpfs-cli/src/components/Files.vue
+++ httpfs-cli/src/components/Files.vue
@@ -1,51 +1,36 @@
-<script lang="ts">
-import { ref, watch, defineComponent } from "vue";
+<script setup lang="ts">
+import { ref, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { downloadFile } from "../services/FilesService";
+
 import Breadcrumb from "./files/Breadcrumb.vue";
 import FilesList from "./files/FilesList.vue";
 import DirectoryMenu from "./files/DirectoryMenu.vue";
 
-export default defineComponent({
-  components: {
-    Breadcrumb,
-    FilesList,
-    DirectoryMenu,
-  },
+const route = useRoute();
+const router = useRouter();
+const path = ref(Array.from(route.params.path));
 
-  setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const path = ref(Array.from(route.params.path));
+const reloadFilesList = ref(false);
 
-    const reloadFilesList = ref(false);
+watch(
+  () => route.params.path,
+  async (newPath) => {
+    path.value = Array.from(newPath);
+  }
+);
 
-    watch(
-      () => route.params.path,
-      async (newPath) => {
-        path.value = Array.from(newPath);
-      }
-    );
-
-    const clickItem = (item: any) => {
-      if (item.directory) {
-        router.push({
-          params: {
-            path: [...path.value, item.name],
-          },
-        });
-      } else {
-        downloadFile(path.value, item.name);
-      }
-    };
-
-    return {
-      path,
-      reloadFilesList,
-      clickItem,
-    };
-  },
-});
+const clickItem = (item: any) => {
+  if (item.directory) {
+    router.push({
+      params: {
+        path: [...path.value, item.name],
+      },
+    });
+  } else {
+    downloadFile(path.value, item.name);
+  }
+};
 </script>
 
 <template>
