6a350e6eda45891925fc18196f24988a6218c11d
yamachi4416 <yamachi4416@gmail.com>
2022-05-01 10:58:49 +0900

    ファイルのリストの日付、サイズのフォーマットを追加

--- httpfs-cli/src/components/files/FilesList.vue
+++ httpfs-cli/src/components/files/FilesList.vue
@@ -34,6 +34,32 @@ watch(
     }
   }
 );
+
+const formatDateTime = (str: string): string => {
+  if (!str) {
+    return "";
+  }
+  const date = new Date(str);
+  const yyyy = date.getFullYear();
+  const MM = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDate()).padStart(2, "0");
+  const hh = String(date.getHours()).padStart(2, "0");
+  const mm = String(date.getMinutes()).padStart(2, "0");
+  const ss = String(date.getSeconds()).padStart(2, "0");
+  return `${yyyy}/${MM}/${dd} ${hh}:${mm}:${ss}`;
+};
+
+const formatSize = (size: number): string => {
+  let s = size;
+  let u = "";
+  for (u of ["B", "KB", "MB", "GB"]) {
+    if (s < 1024) {
+      break;
+    }
+    s = Math.round(s / 1024);
+  }
+  return `${s} ${u}`
+};
 </script>
 
 <template>
@@ -54,9 +80,9 @@ watch(
               }}</a>
             </div>
           </td>
-          <td>{{ item.lastModified }}</td>
+          <td>{{ formatDateTime(item.lastModified) }}</td>
           <td>{{ item.mimeType }}</td>
-          <td>{{ item.directory ? "" : item.size }}</td>
+          <td>{{ item.directory ? "" : formatSize(item.size || 0) }}</td>
         </tr>
       </tbody>
     </table>
