0ab1e62c151ef0d87709f3b1a20a7304a53790a1
yamachi4416 <yamachi4416@gmail.com>
2022-05-08 16:07:51 +0900

    画面の高さの設定にcss変数を利用するように変更

--- httpfs-cli/src/App.vue
+++ httpfs-cli/src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue';
+import { onBeforeMount, onErrorCaptured, onUnmounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { HttpException } from './services/FilesService';
 
 const router = useRouter();
+
+onBeforeMount(() => {
+  const setVH = () => {
+    document.documentElement.style.setProperty(
+      '--vh',
+      `${window.innerHeight}px`
+    );
+  };
+  setVH();
+
+  window.addEventListener('resize', setVH);
+  onUnmounted(() => {
+    window.removeEventListener('resize', setVH);
+  });
+});
 
 onErrorCaptured((err, instalce, info) => {
   console.log(err, instalce, info);
