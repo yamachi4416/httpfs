35f827655992708a283b034b74b60369181dd224
yamachi4416 <yamachi4416@gmail.com>
2022-05-08 16:17:45 +0900

    vAutoFocusでフォーカスする前のフォーカスを復元するように変更

--- httpfs-cli/src/directives/vAutoFocus.ts
+++ httpfs-cli/src/directives/vAutoFocus.ts
@@ -1,9 +1,20 @@
 import { ObjectDirective } from 'vue';
 
-export const vAutoFocus: ObjectDirective<HTMLElement> = {
+interface VAutoFocus extends HTMLElement {
+  _v_auto_focus_state: {
+    prevFocusEl?: HTMLElement;
+  };
+}
+
+export const vAutoFocus: ObjectDirective<VAutoFocus> = {
   mounted(el, { value }) {
+    el._v_auto_focus_state = {};
     if (value === true || value == null) {
-      if (document.activeElement !== el) {
+      const prevFocusEl = document.activeElement;
+      if (prevFocusEl !== el) {
+        if (prevFocusEl instanceof HTMLElement) {
+          el._v_auto_focus_state.prevFocusEl = prevFocusEl;
+        }
         el.focus();
       }
     }
@@ -15,5 +26,12 @@ export const vAutoFocus: ObjectDirective<HTMLElement> = {
         el.focus();
       }
     }
+  },
+  beforeUnmount(el) {
+    const prevFocusEl = el._v_auto_focus_state.prevFocusEl;
+    if (prevFocusEl && document.contains(prevFocusEl)) {
+      prevFocusEl.focus();
+    }
+    delete el._v_auto_focus_state;
   },
 };
